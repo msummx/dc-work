8d0df23b35e2ad9884cf67b9377cfc55f53f3e30
Matt Summers <mtsummersx@gmail.com>
2022-08-03 20:12:07 -0400

    finished evenodd assignment

---
+++ Week1/Aug3/evenodd.py
@@ -0,0 +1,12 @@
+try:
+    numberIn = input('Please input a number: ')
+    number = int(numberIn)
+#Attempts to convert a user input string into integer
+except: 
+    print('Invalid entry.')
+#Catches errors and prevents program from crashing.
+
+if number % 2 == 0:
+    print('The number ' + numberIn + ' is even!')
+else:
+    print('The number ' + numberIn + ' is odd!')
