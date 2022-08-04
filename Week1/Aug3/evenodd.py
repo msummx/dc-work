fac9cd99b2e7294a5314e4d2026bc206f8f12fa5
Matt Summers <mtsummersx@gmail.com>
2022-08-03 20:13:54 -0400

    updated notes in evenodd

--- Week1/Aug3/evenodd.py
+++ Week1/Aug3/evenodd.py
@@ -10,3 +10,4 @@ if number % 2 == 0:
     print('The number ' + numberIn + ' is even!')
 else:
     print('The number ' + numberIn + ' is odd!')
+#Checks if there is a remainder and displays message for each case.
