aade8ef439ec3a8fa24a4b00e3638a45a21a7484
Matt Summers <mtsummersx@gmail.com>
2022-08-08 22:58:57 -0400

    fixed comment formatting

--- Week2/Aug8/reverseprime.py
+++ Week2/Aug8/reverseprime.py
@@ -1,22 +1,22 @@
 #sets a range for numbers to check
 numbers = range(0,100)
 
-reverseOrder = numbers[::-1]        #slices numbers to read backwards
-primeNumbers = []       #initalizes empty list to house prime numbers
-for x in reverseOrder:          #checks numbers 1 by 1 in reverse order
-    isPrime = False             #sets default to False
-    number = int(x)         #saves integer x as a variable to use throughout code
-    if number == 2:     #forces 2 to be appended if it appears
+reverseOrder = numbers[::-1]                    #slices numbers to read backwards
+primeNumbers = []                           #initalizes empty list to house prime numbers
+for x in reverseOrder:                                   #checks numbers 1 by 1 in reverse order
+    isPrime = False                                      #sets default to False
+    number = int(x)                                      #saves integer x as a variable to use throughout code
+    if number == 2:                                         #forces 2 to be appended if it appears
         primeNumbers.append(number)   
-    elif number > 1:        #makes sure the number is positive and higher than 1
-        for y in range(2, number):      #cycles through numbers 2 -> number-1
-            if number % y == 0:     #checks that there are no factors
-                isPrime = False     #keeps isPrime as False
+    elif number > 1:                                 #makes sure the number is positive and higher than 1
+        for y in range(2, number):                            #cycles through numbers 2 -> number-1
+            if number % y == 0:                     #checks that there are no factors
+                isPrime = False                          #keeps isPrime as False
                 break
             else:
-                isPrime = True      #becomes True only if no numbers are factors
+                isPrime = True                          #becomes True only if no numbers are factors
     if isPrime == True:
-        primeNumbers.append(number)     #adds number to prime list
+        primeNumbers.append(number)                   #adds number to prime list
         
             
 
